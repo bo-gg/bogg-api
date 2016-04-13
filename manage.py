4e74b6095032088ffc3befc0ad41c3d85bd35953
Ben Friedland <ben174@gmail.com>
2016-04-12 20:05:16 -0700

    Initial commit

---
+++ manage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    print 'Let\'s try using python -m [command] from now on'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bogg.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
